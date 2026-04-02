eb067db273b5a8fa5d0a74578cadd305fd8654d5
ParkSooBin <bettypark19@gmail.com>
2026-04-02 16:39:00 +0900

    fix:ProfilePage

--- frontend/src/components/ProfileFeed.jsx
+++ frontend/src/components/ProfileFeed.jsx
@@ -1,17 +1,25 @@
-import ProfileTab from "./ProfileTab";
 import SectionTitle from "./SectionTitle";
+import ProfileTab from "./ProfileTab";
 import ProfileCard from "./ProfileCard";
+import ProfileBar from "./ProfileBar";
 
 export function ProfileFeed() {
   return (
     <>
       <SectionTitle />
 
+      <div className="px-4">
         <ProfileTab />
-      <div className="flex">
-        <ProfileCard />
-
+        <div className="grid grid-cols-3 gap-6 p-10">
+          {/* 1. 알림 설정 (기본값) */}
+          <ProfileCard type="alarm" />
+          {/* 2. 계정 설정 */}
+          <ProfileCard type="account" />
+          {/* 3. 시청 설정 */}
+          <ProfileCard type="view" />
         </div>
+        <ProfileBar />
+      </div>
     </>
   );
 }
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,19 +1,21 @@
 import Feed from "../components/Feed";
+import ProfileFeed from "../components/ProfileFeed";
 
 // TODO: 사용자 프로필 페이지 구현
 const ProfilePage = () => {
   return (
-    <div className='text-white px-12 py-16'>
-      <h1 className='text-3xl font-bold'>프로필</h1>
-          <Feed 
-          type='play' // HCard를 사용하는 가로형 피드 타입
-          title='시청 중인 콘텐츠' 
-          sub='이어보기' 
-          items={popularMovies.slice(0, 5)} 
-          mediaType='movie'
-        />
+    <div className="text-white px-12 py-16">
+      <h1 className="text-3xl font-bold">프로필</h1>
+      <Feed
+        type="play" // HCard를 사용하는 가로형 피드 타입
+        title="시청 중인 콘텐츠"
+        sub="이어보기"
+        items={popularMovies.slice(0, 5)}
+        mediaType="movie"
+      />
+      <ProfileFeed />
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
